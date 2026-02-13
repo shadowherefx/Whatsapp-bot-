768277058f52a68a632b033b5cddc69982f39df0
Shadow <shadowhere667788@gmail.com>
2026-02-13 20:56:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
+# main.py
+
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 from bot_handlers import start_handler, verify_handler, menu_handler, pair_handler, dispair_handler, basic_commands
-
 from config import TELEGRAM_BOT_TOKEN
 
 updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
 dp = updater.dispatcher
 
-# ہینڈلرز رجسٹر
+# Handlers رجسٹر
 dp.add_handler(CommandHandler("start", start_handler))
-dp.add_handler(CallbackQueryHandler(verify_handler, pattern="verify"))
+dp.add_handler(CommandHandler("otp", verify_handler))
 dp.add_handler(CommandHandler("menu", menu_handler))
 dp.add_handler(CommandHandler("pair", pair_handler))
 dp.add_handler(CommandHandler("dispair", dispair_handler))
 
-# بیسک کمانڈز
+# Basic commands
 for cmd in basic_commands:
     dp.add_handler(CommandHandler(cmd, basic_commands[cmd]))
 
