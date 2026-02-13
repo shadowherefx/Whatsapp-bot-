e125f09a42ade1abae1540e3e6c6042ca46aa6f4
Shadow <shadowhere667788@gmail.com>
2026-02-13 20:52:59 +0500

    Update Whatsapp_api.py

--- Whatsapp_api.py
+++ Whatsapp_api.py
@@ -1,5 +1,11 @@
+# Whatsapp_api.py
+
 import pywhatkit
 
 def send_otp(number, otp_code):
-    # WhatsApp Web استعمال کرتے ہوئے OTP بھیجیں
+    """
+    WhatsApp Web استعمال کرتے ہوئے OTP بھیجیں
+    number: country code کے ساتھ، جیسے "923001234567"
+    otp_code: OTP number
+    """
     pywhatkit.sendwhatmsg_instantly(f"+{number}", f"Your OTP code is: {otp_code}")
